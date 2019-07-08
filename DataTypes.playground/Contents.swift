874deae82cda56e983b09aa44451251d1cfa7f1d
zozo1013 <zoepunjwani@gmail.com>
2019-07-08 14:59:43 -0700

    message about your work

---
+++ DataTypes.playground/Contents.swift
@@ -0,0 +1,81 @@
+import Cocoa
+
+var str = "Hello, playground"
+var age = 15
+"im \(age) years old"
+var first = "karlie"
+var last = "kloss"
+"\(first)\(last)"
+"\(last)\(first)"
+"\(first) \(last)"
+"\(last) \(first) \(last) \(first)"
+" i heart \(first)"
+var a = 12
+var b = 65
+var c = 31
+var d = 98
+// + addition
+// - subtraction
+// / division
+// * multiplication
+a+b+c+d
+206/4
+(a+b+c+d)/4
+10.25*20
+var name = "antonice"
+" happy birthday to you happy birthday to you happy birthday to \(name) happy birthday to you"
+var stringtocompress =  "antonice"
+5<3
+12>7
+6 != 8
+7 == 7
+var da = 5
+if da < 4 {
+    print("you are a puppy")
+}
+else if da > 13
+{
+    print("you are a elderly ")
+}
+else {
+    print("you are awesome")
+}
+var ff = "tacos"
+
+if ff == "chipotle" {
+    print("they are good")
+}
+else if ff == "starbucks" {
+    print("it's nasty")
+}
+else {
+    print("it will be ok")
+}
+var hasAccount = true
+
+if hasAccount == false {
+    print("please log in")
+}
+else {
+    print("welcome back")
+}
+
+
+
+
+
+
+
+var grade = 10
+
+if grade < 6 {
+    print("elementary")
+}
+
+else if grade > 9 {
+    print("high school")
+}
+
+else {
+    print("middle school")
+}
